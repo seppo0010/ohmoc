daf4176836dcbf630ac1517c2bb617c4b9078f97
Sebastian Waisbrot <seppo0010@gmail.com>
2015-04-09 07:23:25 -0700

    Add batch update test

--- Example/Tests/Batch.m
+++ Example/Tests/Batch.m
@@ -43,6 +43,29 @@ describe(@"batch", ^{
         XCTAssertEqualObjects(posts, expected);
         XCTAssertEqual(post, [posts objectAtIndex:0]);
     });
+    it(@"can update multiple objects", ^{
+        Ohmoc* ohmoc = [Ohmoc instance];
+        @autoreleasepool {
+            [ohmoc multi];
+            OOCPerson* p1 = [OOCPerson create];
+            OOCPerson* p2 = [OOCPerson create];
+            OOCPerson* p3 = [OOCPerson create];
+            [ohmoc exec];
+
+            [ohmoc multi];
+            p1.name = @"Person 1";
+            [p1 save];
+            p2.name = @"Person 2";
+            [p2 save];
+            p3.name = @"Person 3";
+            [p3 save];
+            [ohmoc exec];
+        }
+        XCTAssertFalse([OOCPerson isCached:@"1"]);
+        NSArray* names = [[[[OOCPerson all] arrayValue] valueForKey:@"name"] sortedArrayUsingSelector:@selector(compare:)];
+        NSArray* expected = @[@"Person 1", @"Person 2", @"Person 3"];
+        XCTAssertEqualObjects(names, expected);
+    });
 });
 
 SpecEnd
